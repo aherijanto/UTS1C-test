ff31371ed7cde4419a638fa2cf6e40faf936ab70
bistique <aherijanto@gmail.com>
2023-11-08 19:42:00 +0700

    coding baru

--- src/JawabanUTS.java
+++ src/JawabanUTS.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class JawabanUTS {
+public class JawabanUTS {
+    public static void main(String[] args) {
+        System.out.println("hello github...");
+        for(int i=0;i<10; i++){
+            System.out.println("abc");
+        }
+    }
 }
